265482998e3eb1390d3bb49f40ba94371b1673b6
akashcalil <ashcalili@gmail.com>
2026-07-26 15:51:36 -0400

    chore: export QUIC transports

--- dnstrace/transports/__init__.py
+++ dnstrace/transports/__init__.py
@@ -1,7 +1,17 @@
 from dnstrace.transports.base import Transport
 from dnstrace.transports.doh import DoHTransport
+from dnstrace.transports.doh3 import DoH3Transport
+from dnstrace.transports.doq import DoQTransport
 from dnstrace.transports.dot import DoTTransport
 from dnstrace.transports.tcp import TCPTransport
 from dnstrace.transports.udp import UDPTransport
 
-__all__ = ["Transport", "TCPTransport", "UDPTransport", "DoTTransport", "DoHTransport"]
+__all__ = [
+    "Transport",
+    "TCPTransport",
+    "UDPTransport",
+    "DoTTransport",
+    "DoHTransport",
+    "DoQTransport",
+    "DoH3Transport",
+]
