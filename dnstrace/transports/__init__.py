2e55c1dca495804a90c0a8e738c3dc3d6e4c6d28
akashcalil <ashcalili@gmail.com>
2026-07-26 05:05:55 -0400

    chore: export encrypted DNS transports

--- dnstrace/transports/__init__.py
+++ dnstrace/transports/__init__.py
@@ -1,5 +1,7 @@
 from dnstrace.transports.base import Transport
+from dnstrace.transports.doh import DoHTransport
+from dnstrace.transports.dot import DoTTransport
 from dnstrace.transports.tcp import TCPTransport
 from dnstrace.transports.udp import UDPTransport
 
-__all__ = ["Transport", "TCPTransport", "UDPTransport"]
+__all__ = ["Transport", "TCPTransport", "UDPTransport", "DoTTransport", "DoHTransport"]
