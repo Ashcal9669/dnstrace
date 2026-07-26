e781ade22ef416c9dae67fc4318badbdb2758ecf
akashcalil <ashcalili@gmail.com>
2026-07-25 21:00:12 -0400

    chore: export transport implementations

---
+++ dnstrace/transports/__init__.py
@@ -0,0 +1,5 @@
+from dnstrace.transports.base import Transport
+from dnstrace.transports.tcp import TCPTransport
+from dnstrace.transports.udp import UDPTransport
+
+__all__ = ["Transport", "TCPTransport", "UDPTransport"]
